744d958edf6f4472859d68cf6c60f16b1bb37f81
PauloNayron <paulonayron.s@hotmail.com>
2022-04-05 20:14:59 -0300

    refactor: migrando o log

--- src/main/java/com/nayron/kafkaecommerce/KafkaService.java
+++ src/main/java/com/nayron/kafkaecommerce/KafkaService.java
@@ -10,16 +10,26 @@ import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
 import java.util.UUID;
+import java.util.regex.Pattern;
 
 class KafkaService implements Closeable {
 
     private final KafkaConsumer<String, String> consumer;
     private final ConsumerFunction parse;
 
-    KafkaService(String groupId, String topic, ConsumerFunction parse) {
+    private KafkaService(ConsumerFunction parse, String groupId) {
         this.parse = parse;
-        this.consumer = new KafkaConsumer<String, String>(properties(groupId));
+        this.consumer = new KafkaConsumer<>(properties(groupId));
+    }
+
+    KafkaService(String groupId, String topic, ConsumerFunction parse) {
+        this(parse, groupId);
         consumer.subscribe(Collections.singletonList(topic));
+    }
+
+    KafkaService(String groupId, Pattern topic, ConsumerFunction parse) {
+        this(parse, groupId);
+        consumer.subscribe(topic);
     }
 
     void run() {
